1bb082c2c05fa1d3eec5025283fef6c55b368fe6
Siryu <coreymassey@gmail.com>
2017-09-24 21:04:56 -0600

    modify tests and complicate Run a little

--- internal/services/SomeService.go
+++ internal/services/SomeService.go
@@ -1,7 +1,12 @@
 package services
 
+import "fmt"
+
 type SomeService struct{}
 
 func (s SomeService) Run(st string, in int) (string, int) {
+	if in == 4 {
+		st = fmt.Sprintf("%s%v", st, in)
+	}
 	return st, in
 }
--- internal/services/SomeService_test.go
+++ internal/services/SomeService_test.go
@@ -5,15 +5,24 @@ import "testing"
 func TestRunOldStyle(t *testing.T) {
 	testSt := "test string"
 	someInt := 5
-
 	expectedSt := "test string"
 	expectedInt := 5
+
+	testSt2 := "test string"
+	someInt2 := 4
+	expectedSt2 := "test string4"
+	expectedInt2 := 4
 
 	someService := SomeService{}
 
 	returnString, returnInt := someService.Run(testSt, someInt)
 	if returnString != expectedSt || returnInt != expectedInt {
 		t.Errorf("expected '%s', '%v' but got '%s', '%v'", expectedSt, expectedInt, returnString, returnInt)
+	}
+
+	returnString2, returnInt2 := someService.Run(testSt2, someInt2)
+	if returnString2 != expectedSt2 || returnInt2 != expectedInt2 {
+		t.Errorf("expected '%s', '%v' but got '%s', '%v'", expectedSt2, expectedInt2, returnString2, returnInt2)
 	}
 }
 
@@ -31,6 +40,13 @@ func TestRunNewStyle(t *testing.T) {
 			5,
 			"test string",
 			5,
+		},
+		{
+			"Return back 4 appended to the end of st if in is 4",
+			"test string",
+			4,
+			"test string4",
+			4,
 		},
 	}
 
